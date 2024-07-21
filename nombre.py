c1e8d9062094c395dad9e2dc011a5a1434c12cfb
valdez6555 <e23067162@icepcasserole.com.mx>
2024-07-21 11:10:04 -0600

    Add files via upload

---
+++ nombre.py
@@ -0,0 +1,9 @@
+def main():
+    # Solicita al usuario que ingrese su nombre
+    nombre = input("¡Hola! ¿Cuál es tu nombre? ")
+
+    # Imprime un mensaje de saludo personalizado
+    print(f"¡Hola, {nombre}! Bienvenido(a) al mundo de la programación.")
+
+if __name__ == "__main__":
+    main()
